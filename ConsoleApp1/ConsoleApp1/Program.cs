00419a6a128245cf762558aad69fa7552602530a
Samuel Kuta <kuta@SPSEJECNANET.CZ>
2023-01-30 09:09:09 +0100

    Kuta Samuel C3b

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder consStringBuilder = new SqlConnectionStringBuilder();
+            consStringBuilder.UserID = "sa";
+            consStringBuilder.Password = "student";
+            consStringBuilder.InitialCatalog = "test";
+            consStringBuilder.DataSource = "PC975";
+            consStringBuilder.ConnectTimeout = 30;
+            SqlConnection connection;
+
+            Textnode? node1 = Textnode.LoadNodeFromDB(3, consStringBuilder.ConnectionString);
+
+            Console.WriteLine(node1);
+
+            Textnode node2 = new Textnode(0, "Node Insertion text","NodeNameNew");
+            Textnode.InsertIntoDB(node2,consStringBuilder.ConnectionString);
+
+            List<Textnode>? nodes = Textnode.LoadAllNodes(consStringBuilder.ConnectionString);
+            nodes.ForEach(node => Console.WriteLine(node));
+ 
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Textnode.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    internal class Textnode
+    {
+        public int ID { get; set; }
+
+        public string text;
+
+        public string nodeName;
+
+        public Textnode(int iD, string text, string nodeName)
+        {
+            
+            this.ID = iD;
+            this.text = text;
+            this.nodeName = nodeName;
+
+        }
+
+
+
+
+
+
+
+
+        public override string ToString()
+        {
+            return "Id="+ID+" text='"+text+"'"+" nodename='"+this.nodeName+"'";
+        }
+
+        public static void InsertIntoDB(Textnode tn,string connectionString)
+        {
+            try
+            {
+                SqlConnection connection;
+                using (connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection established");
+                    string query = string.Format("insert into deez(text,nodename) values('{0}','{1}')", tn.text, tn.nodeName);
+                    
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+
+                    }
+                }
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static Textnode? LoadNodeFromDB(int id,string connectionString)
+        {
+            Textnode tn = null;
+            try
+            {
+                SqlConnection connection;
+                using (connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection established");
+                    string query = string.Format("select * from deez where deez.id = {0}", id);
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        
+                        SqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            
+                            if (reader[0].ToString() == null || reader[1].ToString() == null) { return null; } 
+                            tn = new Textnode(Int32.Parse(reader[0].ToString()), reader[1].ToString(), reader[2].ToString());
+                            
+                        }
+                    }
+                }
+
+                return tn;
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return tn;
+        }
+
+
+        public static List<Textnode>? LoadAllNodes(string connectionString)
+        {
+            List<Textnode> list = new List<Textnode>();
+
+            try
+            {
+                SqlConnection connection;
+                using (connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection established");    
+                    string query = string.Format("select * from deez");
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        
+                        SqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            
+                            if (reader[0].ToString() == null || reader[1].ToString() == null) { return list; }
+                            Textnode tn = new(Convert.ToInt32(reader[0]), reader[1].ToString(), reader[2].ToString());
+
+                            list.Add(tn);
+
+                        }
+                    }
+                }
+
+                return list;
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return null;
+        }
+
+
+
+    }
+}
